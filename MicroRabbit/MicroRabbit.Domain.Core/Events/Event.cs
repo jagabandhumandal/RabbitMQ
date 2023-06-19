78677ebec71e861d91f1f284806d259a8072ec64
jagabandhu mandal <creativeyes07@gmail.com>
2023-06-19 20:47:53 +0530

    implementation of interface and class od domain

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Events/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    internal abstract class Event
+    {
+        public string MessageType { get; protected set; }
+
+        public Event()
+        {
+            MessageType = GetType().Name;
+        }
+    }
+}
